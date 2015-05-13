1131b5398d289d7389d1a6abeea834970e502eae
ahmed <ahmednasser1993@gmail.com>
2015-05-13 16:22:51 +0200

    still unfix protofolio

--- app/scripts/controllers/Portofolio/portofolio.js
+++ app/scripts/controllers/Portofolio/portofolio.js
@@ -2,18 +2,42 @@
 var MainCtrl = angular.module('clientApp');
 
 MainCtrl.controller('PortofolioCtrl', function ($scope , $timeout) {
-	$scope.project = {
-		[
-			name : "reversi",
-			programming : "C++",
-			link : "https://github.com/sngvahmed/Reversi"
-			description "description"
-		],
-		[
-			name : "reversi",
-			programming : "C++",
-			link : "https://github.com/sngvahmed/Reversi"
-			description "description"
-		]
+	
+	$scope.project = [
+		{
+			"name" : "reversi",
+			"programming" : "C++",
+			"link" : "https://github.com/sngvahmed/Reversi",
+			"description" : "description",
+			"rel" : "1"
+		}
+	];
+
+	var tmp = function(){
+		console.log("size is " + $scope.project.length);
 	}
-})
+	tmp();
+});
+
+// ,
+// 		{
+// 			"name" : "reversi",
+// 			"programming" : "C++",
+// 			"link" : "https://github.com/sngvahmed/Reversi",
+// 			"description" : "description",
+// 			"rel" : "2"
+// 		},
+// 		{
+// 			"name" : "reversi",
+// 			"programming" : "C++",
+// 			"link" : "https://github.com/sngvahmed/Reversi",
+// 			"description" : "description",
+// 			"rel" : "3"
+// 		},
+// 		{
+// 			"name" : "reversi",
+// 			"programming" : "C++",
+// 			"link" : "https://github.com/sngvahmed/Reversi",
+// 			"description" : "description",
+// 			"rel" : "4"
+// 		}
